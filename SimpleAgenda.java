2e0195532e5de9c21d9087f8b3afa9372b27087c
Samuelrtiz <sjh110125@gmail.com>
2024-01-24 20:37:41 -0400

    Primera version de la aplicacion

---
+++ SimpleAgenda.java
@@ -0,0 +1,25 @@
+public class SimpleAgenda {
+
+
+    public static void agregarContacto(String a) {
+
+    }
+
+    public static void removerContacto(int a) {
+
+    }
+
+    public static String actualizarContacto(int a) {
+        return "N/A";
+    }
+
+    public static void mostrarContactos() {
+
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
