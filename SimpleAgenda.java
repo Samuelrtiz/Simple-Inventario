3a00fbcc23c6c51b05d089e4ad4e8bb7f5996a10
samuelrtiz <sjh110125@gmail.com>
2024-02-16 09:34:48 -0400

    Version 2 del programa

--- SimpleAgenda.java
+++ SimpleAgenda.java
@@ -1,11 +1,32 @@
+import java.util.Scanner;
+
 public class SimpleAgenda {
+    public static void main(String[] args) {
 
+        removerContacto(1250);
+    }
+    public static void agregarContacto() {
+        String contacts;
+        Scanner sc = new Scanner(System.in);
 
-    public static void agregarContacto(String a) {
+        System.out.println("Ingrese un nuevo contacto a agregar");
+        contacts = sc.next();
 
+        if (contacts.length() >= 4 && contacts.length() <= 8) {
+            System.out.println("El Contacto fue agregado con éxito");
+        } else if (contacts.length() > 8 && contacts.length() <= 30) {
+            System.out.println(" Agregado, contiene entre 8-30 caracteres");
+        } else {
+            System.out.println("el Nombre es muy largo");
+        }
     }
 
-    public static void removerContacto(int a) {
+    public static void removerContacto(int id) {
+        if (id >= 1000 && id <= 9999) {
+            System.out.println("Verificando contacto a eliminar..");
+        } else {
+            System.out.println("el ID no es valido");
+        }
 
     }
 
@@ -14,11 +35,7 @@ public class SimpleAgenda {
     }
 
     public static void mostrarContactos() {
-
-    }
-
-
-    public static void main(String[] args) {
+        System.out.println("Mostrando contactos..");
 
     }
 
